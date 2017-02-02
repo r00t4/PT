e84e29369ce477037a8e37d24f64abd2186d8138
Kazbek <kazbekborash@gmail.com>
2017-02-03 00:27:12 +0600

    Comment

---
+++ LAB1.2/LAB1.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1._2
+{
+    class Student
+    {
+        public string name, surname;
+        public int age;
+        public double gpa;
+        public Student(string name, string surname, int age, double gpa)
+        {
+            this.age = age;
+            this.name = name;
+            this.surname = surname;
+            this.gpa = gpa;
+        }
+        public override string ToString()
+        {
+            return name + " " + surname + " " + age + " " + gpa;
+        }
+
+    }
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Student s = new Student("LOL", "Kek", 19, 2.9);
+            Console.WriteLine(s.name);
+            Console.WriteLine(s.age);
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ LAB1.2/LAB1.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LAB1.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("LAB1.2")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("dc1e0799-0735-4fab-8c3b-0ba41fbbb1cb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
