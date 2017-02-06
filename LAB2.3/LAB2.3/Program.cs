18adb2c4b43e3a9953b09cc681f0d5433b23264a
Kazbek <kazbekborash@gmail.com>
2017-02-06 17:21:25 +0600

    Commit

--- LAB2.3/LAB2.3/Program.cs
+++ LAB2.3/LAB2.3/Program.cs
@@ -11,33 +11,73 @@ namespace LAB2._3
     {
         static void Main()
         {
-            string text = File.ReadAllText(@"C:\FTP\WriteText.txt");
-            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            FileStream text = new FileStream(@"C:\FTP\WriteText.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(text);
+            int ger;
+            int max = -1000;
+            int min = 10000;
+            int r = 1;
+            while (!sr.EndOfStream)
+            {
+                ger = Convert.ToInt16(sr.ReadLine());
+                for (int i = 2; i < ger; i++)
+                {
+                    if (ger % i == 0)
+                    {
+                        r = i;
+                        break;
+                    }
+                }
+                if (r == 1)
+                {
+                    if (ger < min)
+                    {
+                        min = ger;
+                    }
+                    if (ger > max)
+                    {
+                        max = ger;
+                    }
+                }
+            }
+            sr.Close();
+            
+            /*FileStream text = new FileStream(@"C:\FTP\WriteText.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(text);
 
             int min = 10000;
             int max = -1000;
             for (int i = 0; i < text.Length; i++)
             {
+                int r = 1;
                 int g = Convert.ToInt16(text[i]);
-                for (int j = 2; j <= g; j++)
+                for (int j = 2; j < g; j++)
                 {
                     if (g % j == 0)
                     {
-                        if (g < min)
-                        {
-                            min = g;
-                        }
-                        if (g > max)
-                        {
-                            max = g;
-                        }
-                        break;  
+                        r = g;
+                        break;
                     }
-
                 }
-            }
+                if (r == 1)
+                {
+                    if (max < g)
+                    {
+                        max = g;
+                    }
+                    if (min > g)
+                    {
+                        min = g;
+                    }
+                }
+            }*/
             Console.WriteLine("Min is " + min);
             Console.WriteLine("Max is " + max);
+            using (StreamWriter file = new StreamWriter(@"C:\FTP\WriteLines2.txt"))
+            {
+                file.WriteLine("Min is " + min);
+                file.WriteLine("Max is " + max);
+            }
             Console.ReadKey();
         }
     }
